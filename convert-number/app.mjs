86acde0c42cbfe2877f1aebdcf4a111875455496
Cameron <cameronpeck317@gmail.com>
2023-01-25 15:08:58 -0500

    add callback and resultMap per AWS documentation to return results as a simple string map

--- convert-number/app.mjs
+++ convert-number/app.mjs
@@ -1,25 +1,20 @@
 
 
-export const lambdaHandler = async (event, context) => {
+export const lambdaHandler = async (event, context, callback) => {
     console.log('event: ', event);
     console.log('context: ', context);
     const phoneNumber = event['Details']['ContactData']['CustomerEndpoint']['Address'];
     const vanityNumbers = getVanityNumbers(phoneNumber);
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: `Your top 3 vanity numbers are ${vanityNumbers} and number was ${phoneNumber}`
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+    let resultMap = {
+        phoneNumber: phoneNumber,
+        vanityNumbers: vanityNumbers
+    };
+
+    callback(null, resultMap);
 };
 
 function getVanityNumbers (string) {
-    // code that converts the number here //
     const result = ['1-800-CALL-CAM', '1-800-CALL-HIM', '1-800-CALL-HER' ];
+    // code that converts the number here //
     return result;
 }
