89900fee42f76b50791a0f4a4dc4bd7786f1688d
Cameron <cameronpeck317@gmail.com>
2023-01-25 14:57:16 -0500

    add actual test data with same format as Amazon Connect request event and ensure phone number arrives to the lambda function

--- convert-number/app.mjs
+++ convert-number/app.mjs
@@ -1,13 +1,15 @@
 
 
 export const lambdaHandler = async (event, context) => {
-    const phoneNumber = event['phoneNumber'];
+    console.log('event: ', event);
+    console.log('context: ', context);
+    const phoneNumber = event['Details']['ContactData']['CustomerEndpoint']['Address'];
     const vanityNumbers = getVanityNumbers(phoneNumber);
     try {
         return {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: `Your top 3 vanity numbers are ${vanityNumbers}`
+                message: `Your top 3 vanity numbers are ${vanityNumbers} and number was ${phoneNumber}`
             })
         }
     } catch (err) {
@@ -17,6 +19,7 @@ export const lambdaHandler = async (event, context) => {
 };
 
 function getVanityNumbers (string) {
+    // code that converts the number here //
     const result = ['1-800-CALL-CAM', '1-800-CALL-HIM', '1-800-CALL-HER' ];
     return result;
 }
